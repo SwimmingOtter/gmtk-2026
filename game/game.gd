ff41a35e73364c5e5ea74e4558958b6af2336bce
JBBardin <bardin.jeanbaptiste78@gmail.com>
2026-07-25 00:16:50 +0200

    feat: Juice up the monkeys

--- game/game.gd
+++ game/game.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
+@export var monkey_scene: PackedScene
+@export var monkey_count: int = 10
+@onready var monkeys: Node2D = $CanvasLayer/Monkeys
 
 func _ready() -> void:
 	for monkey in get_tree().get_nodes_in_group("monkey"):
-		if randf() < 0.5:
-			monkey.position = Vector2(randf_range(200, 1700), randf_range(100, 360))
-		else:
-			monkey.position = Vector2(randf_range(200, 1700), randf_range(700, 900))
+		monkey.free()
+
+	for i in range(monkey_count):
+		var monkey = monkey_scene.instantiate()
+		monkeys.add_child(monkey)
+		var angle: int = randi() % 360 - 180
+		var direction = Vector2(cos(angle * PI / 180), sin(angle * PI / 180))
+		direction = direction.normalized()
+		monkey.position = Vector2(960, 540) + direction * randf_range(200, 400)
---
+++ game/monkeys/monkey.gd
@@ -0,0 +1,65 @@
+class_name Monkey extends CharacterBody2D
+
+enum STATE {
+	IDLE,
+	WAITING,
+	TALKING,
+	MOVING
+}
+
+@onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
+@onready var timer: Timer = %Timer
+var state: STATE = STATE.IDLE
+var direction: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	timer.timeout.connect(update_state)
+	animated_sprite_2d.frame = randi() % 10
+	if randf() > 0.5:
+		animated_sprite_2d.flip_h = true
+	idle()
+	
+
+func update_state() -> void:
+	if state == STATE.IDLE:
+		if randi() % 4 == 0:
+			move()
+		elif randi() % 20 == 0:
+			talk()
+		else:
+			timer.start(randf_range(0.8, 1.2))
+	else:
+		idle()
+
+func _physics_process(_delta):
+	if state == STATE.MOVING:
+		velocity = direction * 100
+		move_and_slide()
+			
+func move():
+	state = STATE.MOVING
+	var angle: int = randi() % 360
+	direction = Vector2(cos(angle / (2 * PI)), sin(angle / (2 * PI))) * 150
+	direction = direction.normalized()
+	if direction.x > 0:
+		animated_sprite_2d.flip_h = true
+	else:
+		animated_sprite_2d.flip_h = false
+	animated_sprite_2d.play("walk")
+
+	timer.start(randf_range(0.8, 1.2))
+	
+	
+func idle():
+	state = STATE.IDLE
+	animated_sprite_2d.play("idle")
+	update_state()
+
+func talk():
+	if animated_sprite_2d.flip_h:
+		%BubbleSprite.flip_h = true
+		%BubbleSprite.position.x = 54
+	else:
+		%BubbleSprite.position.x = -54
+		
+	%BubbleSprite.play("default")
